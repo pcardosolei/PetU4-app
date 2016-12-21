4ba3586be820d2c255cd4a7eedb0d97f1b035a1e
Rafael Antunes <rafael.uminho@hotmail.com>
2016-12-21 19:37:54 +0000

    Upa!

---
+++ Pet4U/app/src/main/java/pet4u/pet4u/managers/RegisterManager.java
@@ -0,0 +1,66 @@
+package pet4u.pet4u.managers;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import pet4u.pet4u.services.RegisterService;
+import pet4u.pet4u.UserDTO;
+import pet4u.pet4u.AppProperties;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RegisterManager {
+    private static RegisterManager ourInstance;
+    private Retrofit retrofit;
+    private Context context;
+    private RegisterService registerService;
+
+    private RegisterManager(Context cntxt) {
+        context = cntxt;
+        retrofit = new Retrofit.Builder()
+                .baseUrl(AppProperties.baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+
+                .build();
+
+        registerService = retrofit.create(RegisterService.class);
+    }
+
+    public static RegisterManager getInstance(Context cntxt) {
+        if (ourInstance == null) {
+            ourInstance = new RegisterManager(cntxt);
+        }
+
+        ourInstance.context = cntxt;
+
+        return ourInstance;
+    }
+
+    /* POST - REGISTER ACCOUNT */
+
+    public synchronized void registerAccount(final RegisterCallback registerCallback, UserDTO userDTO) {
+        Call<Void> call = registerService.registerAccount(userDTO);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                int code = response.code();
+
+                if (code == 200 || code == 201) {
+                    Toast.makeText(context, "User created", Toast.LENGTH_LONG);
+
+                } else {
+                    registerCallback.onFailure(new Throwable("ERROR" + code + ", " + response.raw().message()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                //todo: tratar
+            }
+        });
+    }
+}
