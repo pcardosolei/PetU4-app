3132b34a2e619ed59ffa81f4c5523dc668421ef3
Rafael Antunes <rafael.uminho@hotmail.com>
2017-01-11 17:34:05 +0000

    update

--- Pet4U/app/src/main/java/pet4u/pet4u/AppProperties.java
+++ Pet4U/app/src/main/java/pet4u/pet4u/AppProperties.java
@@ -5,9 +5,9 @@ package pet4u.pet4u;
  */
 
 public class AppProperties {
-    public static String clientId = "pet4uapp";
+    public static String clientId = "appapp";
     public static String clientSecret = "my-secret-token-to-change-in-production";
     public static String grantType = "password";
     public static String scope = "read write";
-    public static String baseUrl = "http://139.59.210.86:8080";
-        }
+    public static String baseUrl = "http://172.26.39.193:8080";
+}
--- Pet4U/app/src/main/java/pet4u/pet4u/LoginActivity.java
+++ Pet4U/app/src/main/java/pet4u/pet4u/LoginActivity.java
@@ -75,10 +75,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Intent intent = getIntent();
+        //Intent intent = getIntent();
 
         // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.username);
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
@@ -93,7 +93,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = (Button) findViewById(R.id.user_log_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -215,13 +215,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
+        //TODO: Replace this with your own logic email.contains("@")
+        return true;
     }
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return password.length() > 1;
     }
 
     /**
@@ -268,9 +268,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                         ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
 
                 // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
+                ContactsContract.Contacts.Data.MIMETYPE + " = ?", new String[]{ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE},
 
                 // Show primary email addresses first. Note that there won't be
                 // a primary email address if the user hasn't specified one.
