0a9b131508bce63d2155dc1369a1a53f7ba50d16
NunoOliv <nuno.oliv.94@gmail.com>
2017-01-30 15:42:30 +0000

    Fixed Wrong Names

--- Pet4U/app/src/main/java/pet4u/pet4u/Activities/PlanoVacinacao.java
+++ Pet4U/app/src/main/java/pet4u/pet4u/Activities/PlanoVacinacao.java
@@ -1,4 +1,4 @@
-package pet4u.pet4u;
+package pet4u.pet4u.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import pet4u.pet4u.R;
 
 public class PlanoVacinacao extends AppCompatActivity {
 
