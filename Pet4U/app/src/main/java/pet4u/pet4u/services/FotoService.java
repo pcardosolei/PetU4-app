8f2d0ecadbe82ea43b0e6367a9b03864e4544154
Rafael Antunes <rafael.uminho@hotmail.com>
2017-02-01 16:58:51 +0000

    foto classes added

---
+++ Pet4U/app/src/main/java/pet4u/pet4u/services/FotoService.java
@@ -0,0 +1,23 @@
+package pet4u.pet4u.services;
+
+import pet4u.pet4u.user.ClientDTO;
+import pet4u.pet4u.user.FotoDTO;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
+
+/**
+ * Created by Rafael on 01/02/2017.
+ * Package: pet4u.pet4u.services.
+ * Project: Pet4U.
+ */
+
+public interface FotoService {
+
+    /*@POST("/api/clientes/{id}")
+    Call<FotoDTO> getCliente(
+            @Header("Authorization") String Authorization,
+            @Path("id") int id
+    );*/
+}
