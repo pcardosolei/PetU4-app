60fc90b104f938dce8f3bf09bdc4b29b18ad5b75
Rafael Antunes <rafael.uminho@hotmail.com>
2017-02-01 15:25:09 +0000

    bugfix

--- Pet4U/app/src/main/java/pet4u/pet4u/AppProperties.java
+++ Pet4U/app/src/main/java/pet4u/pet4u/AppProperties.java
@@ -9,5 +9,7 @@ public class AppProperties {
     public static String clientSecret = "my-secret-token-to-change-in-production";
     public static String grantType = "password";
     public static String scope = "read write";
-    public static String baseUrl = "http://128.199.53.78:8080/";
+    public static String baseUrl = "http://192.168.43.242:8080/";
+
+    // main server : http://128.199.53.78:8080/
 }
