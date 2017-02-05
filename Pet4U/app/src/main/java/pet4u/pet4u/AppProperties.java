32640b729a44524de6c9b7858c5837e9bf2eeeae
Rafael Antunes <rafael.uminho@hotmail.com>
2017-02-05 22:34:32 +0000

    Alteração do IP- fix

--- Pet4U/app/src/main/java/pet4u/pet4u/AppProperties.java
+++ Pet4U/app/src/main/java/pet4u/pet4u/AppProperties.java
@@ -9,7 +9,7 @@ public class AppProperties {
     public static String clientSecret = "my-secret-token-to-change-in-production";
     public static String grantType = "password";
     public static String scope = "read write";
-    public static String baseUrl = "http://192.168.1.64:8080/";
+    public static String baseUrl = "http://128.199.53.78:8080/";
 
     // main server : http://128.199.53.78:8080/
     // server do carlos: http://192.168.43.242:8080/
